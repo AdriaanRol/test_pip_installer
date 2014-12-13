5af67ddbc5004ef956ce04c2a42920770d7f9548
Adriaan <adriaan.rol@gmail.com>
2014-12-13 14:35:15 +0100

    Added setup py and a module

---
+++ mymodule.py
@@ -0,0 +1,5 @@
+def print_hello_world():
+    '''
+    A trivial hello world function
+    '''
+    print 'Hello world!'
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='test',
+    version='1.0',
+    description='Test pip install ',
+    author='Adriaan Rol',
+    author_email='adriaan.rol@gmail.com',
+    url='https://github.com/AdriaanRol/test_pip_installer',
+    # packages=['distutils', 'distutils.command'],
+)
